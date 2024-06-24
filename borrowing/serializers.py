1b1918de9142a3de54b6c40a0abd381637e3d5f6
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-06-24 17:01:21 +0200

    Add CRUD for Borrowing

---
+++ borrowing/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from books.serializers import BookSerializer
+from borrowing.models import Borrowing
+from user.serializers import UserSerializer
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    book = BookSerializer(read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "user",
+            "book",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+        )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "user", "book", "expected_return_date")
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer, BorrowingCreateSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return BorrowingSerializer
+        return BorrowingCreateSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Borrowing.objects.all()
+        return Borrowing.objects.filter(user_id=user.id)
