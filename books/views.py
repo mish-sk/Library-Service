827947cec94366166d79658321757c6d0916b065
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-06-26 10:29:50 +0200

    Add swagger documentation to the Book endpoints

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiParameter
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 
@@ -5,6 +6,40 @@ from books.models import Book
 from books.serializers import BookSerializer
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="title",
+                description="Filter by book title (ex. ?title=book)",
+                required=False,
+                type={"type": "string"},
+            ),
+            OpenApiParameter(
+                name="author",
+                description="Filter by book author(ex. ?author=name)",
+                required=False,
+                type={"type": "string"},
+            ),
+        ],
+        description="Retrieve a list of all books with filtering by title and/or author",
+    ),
+    retrieve=extend_schema(
+        description="Retrieve a specific book by id",
+    ),
+    create=extend_schema(
+        description="Create a new book",
+    ),
+    update=extend_schema(
+        description="Update an existing book",
+    ),
+    partial_update=extend_schema(
+        description="Partially update an existing book",
+    ),
+    destroy=extend_schema(
+        description="Delete a book",
+    ),
+)
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
